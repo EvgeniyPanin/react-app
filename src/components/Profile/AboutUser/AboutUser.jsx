de3eb81acf4b020ee656d65664b60771115d1aeb
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-09-15 15:23:41 +0400

    набросал структуру контактов и принцип добавления иконок

--- src/components/Profile/AboutUser/AboutUser.jsx
+++ src/components/Profile/AboutUser/AboutUser.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import style from "./AboutUser.module.css";
 
-
-
-const AboutUser = ({user}) => {
-    console.log(user)
-    return (
-        <div>
-            <p className={style.paragraph}><b>Обо мне: </b>{user.aboutMe}</p> 
-            <p className={style.paragraph}><b>В поиске работы: </b>{user.lookingForAJob ? '✅' : '❌'}</p> 
-            <p className={style.paragraph}><b>Ключевые навыки: </b>{user.lookingForAJobDescription}</p>
-            <b>Мои контакты: </b><ul>
-                {Object.keys(user.contacts).map(key => {
-                    return user.contacts[key] && <li key={key}>{user.contacts[key]}</li>
-                })}
-            </ul>
-        </div>
-    )
-}
+const AboutUser = ({ user }) => {
+  console.log(user);
+  return (
+    <div>
+      <p className={style.paragraph}>
+        <b>Обо мне: </b>
+        {user.aboutMe}
+      </p>
+      <p className={style.paragraph}>
+        <b>В поиске работы: </b>
+        {user.lookingForAJob ? "✅" : "❌"}
+      </p>
+      <p className={style.paragraph}>
+        <b>Ключевые навыки: </b>
+        {user.lookingForAJobDescription}
+      </p>
+      <b>Мои контакты: </b>
+      <ul className={style.contacts}>
+        {Object.keys(user.contacts).map((key) => {
+          return (
+            <li className={style.contacts__item} key={key}>
+              <a className={`${style[key]} ${style.contacts__link}`}></a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default AboutUser;
